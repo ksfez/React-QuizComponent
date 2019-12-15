4e47a220ccb570d01e165fa8e66dee152daa36d0
Keren <sfez.keren@gmail.com>
2019-12-15 08:45:49 +0200

    first module commit

---
+++ src/Quiz.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+let quizData=require('./quiz_data.json')
+
+class Quiz extends Component {
+
+    render() {
+        return (
+        <div className='QuizQuestions'>{quizData.quiz_questions[0].instruction_text}</div>
+        );
+    }
+    constructor(props) {
+        super(props);
+        this.state={quiz_position : 1}
+    }
+};
+
+export default Quiz;
